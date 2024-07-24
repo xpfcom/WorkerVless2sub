e56a4b6e14cb750f570ce35103c4d6e3bce45846
xpfcom <159119993+xpfcom@users.noreply.github.com>
2024-07-24 19:56:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -39,8 +39,7 @@ let link = '';
 let edgetunnel = 'ed';
 let RproxyIP = 'false';
 let proxyIPs = [//无法匹配到节点名就随机分配以下ProxyIP域名
-	'proxyip.multacom.fxxk.dedyn.io',
-	'proxyip.vultr.fxxk.dedyn.io',
+
 ];
 let CMproxyIPs = [
 	//'proxyip.aliyun.fxxk.dedyn.io:HK',//匹配节点名, 有HK就分配该ProxyIP域名
